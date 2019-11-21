0646a204ea30b5b4f94b0a02ec0789ff3916ead9
jakesavage22 <savagejake22@gmail.com>
2019-11-21 15:19:19 -0500

    here

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
+
+
 // const elements = require("./elements");
 const wrapper = document.querySelector(".wrapper");
+
+const createdHeaderContainer = document.createElement("header");
+createdHeaderContainer.classList.add("header__container");
+wrapper.append(createdHeaderContainer);
+
+const createdHeaderLeftSide = document.createElement("left-side")
 
 
 const createdElement = document.createElement("h1");
 createdElement.innerHTML = "Recycle Made Easy";
-createdElement.classList.add("h1");
+createdElement.classList.add("header__title");
 wrapper.append(createdElement);
 
