0649f8532d1f077eb55a63dc23bf4abda7596dec
robby <robby.remmenga@gmail.com>
2019-12-06 10:14:58 -0500

    tookout menu button

--- src/components.js
+++ src/components.js
@@ -33,81 +33,6 @@ module.exports = {
             document.querySelector(".content-wrapper").innerHTML = "";
             UserPage.displayUserPage();
         }
-
-        this.navigation();
-        // this.login();
-    },
-
-    login() {
-
-        document.querySelector(".nav__button").style.display = "none";
-        const loginButton = document.createElement("button");
-        loginButton.classList.add("login__button");
-        loginButton.textContent = "Login";
-        navContainer.append(loginButton);
-
-        loginButton.onclick = () => {
-            // loginButton.hidden = true;
-            const login = document.querySelector(".nav__button").style.display = "none";
-            if (login.style.display === "block") {
-                login.style.display = "none";
-            } else {
-                login.style.display = "block";
-            }
-        }
-
-    },
-
-
-    navigation() {
-
-        const hrs = document.querySelector(".header__right-side");
-        const navContainer = document.createElement("div");
-        navContainer.classList.add("nav__container");
-        hrs.append(navContainer);
-
-        const links = document.createElement("div");
-        links.classList.add("nav__list");
-        navContainer.append(links);
-
-        const aLink2 = document.createElement("a");
-        aLink2.classList.add("nav__list-item");
-        aLink2.textContent = "Recycle Centers";
-        aLink2.href = "javascript:void(0);";
-        aLink2.onclick = () => {
-            document.querySelector(".content-wrapper").innerHTML = "";
-            recycle_centers.displayRecycleCentersPage();
-        }
-        links.append(aLink2);
-
-        const aLink3 = document.createElement("a");
-        aLink3.classList.add("nav__list-item");
-        aLink3.textContent = "Recycle Categories";
-        aLink3.href = "javascript:void(0);";
-        aLink3.onclick = () => {
-            document.querySelector(".content-wrapper").innerHTML = "";
-            AdminCat.adminCategories();
-        }
-        links.append(aLink3);
-
-        const navButton = document.createElement("button");
-        navButton.classList.add("nav__button");
-        navButton.textContent = "Menu";
-        navContainer.append(navButton);
-
-        // start with nav hidden
-        links.style.display = "none"
-
-        navButton.onclick = () => {
-            const nav = document.querySelector(".nav__list")
-            if (nav.style.display === "block") {
-                nav.style.display = "none";
-            } else {
-                nav.style.display = "block";
-            }
-        }
-
-
     },
 
     async addresses(endpoint) {
@@ -141,6 +66,4 @@ module.exports = {
             addressContainer.append(div);
         })
     }
-
-
 }
